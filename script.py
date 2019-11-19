21459017513b1d5a4feffeff7497768138d92bea
Chris Williams <cwill833@gmail.com>
2019-11-19 13:03:35 -0600

    game prints out the optimal amount of moves it should take you solve game based on your disks input

--- script.py
+++ script.py
@@ -11,7 +11,16 @@ right_stack = Stack('Right')
 
 stack.extend([left_stack, middle_stack, right_stack])
 
+# grab input from user and makes sure it is >= 3 as this is the minimum amount of disks to have a fun game
 num_disks = int(input("\nHow many disks do you want to play with?\n"))
 
 while num_disks < 3:
    num_disks = int(input("Enter a number greater than or equal to 3\n"))
+
+# created first stack with users input
+for num in range(num_disks, 0, -1):
+    left_stack.push(num)
+
+num_optimal_moves = 2^num_disks - 1
+
+print(f"\nThe fastest you can solve this game is in {num_optimal_moves} moves")
